7fcac9cb13638a9535cc584fcfe01d7c3439c716
mbocianski <miles.a.bocianski@gmail.com>
2022-11-29 21:52:18 -0800

    minor updates

--- front-end/src/dashboard/ReservationsDash.js
+++ front-end/src/dashboard/ReservationsDash.js
@@ -100,8 +100,7 @@ export default function ReservationsDash({ reservations, loadDashboard }) {
               </div>
               <div className="col-3 col-md-2 my-auto">
                 <span
-                  className={`badge ${badgeColor} badge-pill p-3
-                  data-reservation-id-status={reservation_id}`}
+                  className={`data-reservation-id-status=${reservation_id}`}
                 >
                   {resStatus}
                 </span>
