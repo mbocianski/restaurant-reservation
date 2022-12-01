70b9feeb2821caf0f85fad0130ac6b5e4c0193e5
mbocianski <miles.a.bocianski@gmail.com>
2022-12-01 13:06:50 -0800

    fixed moment formatt issues

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -46,10 +46,10 @@ function hasProperty(property) {
 //checks to see if date and time exist
 function checkDate(req, res, next) {
   const { reservation_date, reservation_time } = res.locals;
-  const dayName = moment(reservation_date).format("dddd");
+  const dayName = moment(reservation_date, "YYYY-MM-DD", true).format("dddd");
 
-  //reconstitues date and time for comparison to currnt monent
-  const requestedMoment = reservation_date + " " + reservation_time;
+  //reconstitues date and time for comparison to current monent
+  const requestedMoment = moment(reservation_date + " " + reservation_time, moment.ISO_8601);
   if (!moment(reservation_date, "YYYY-MM-DD", true).isValid()) {
     next({
       status: 400,
@@ -75,11 +75,11 @@ function checkDate(req, res, next) {
 //formats time with moment and checks for several time validations
 function checkTime(req, res, next) {
   const { reservation_time } = res.locals;
-  const format = "hh:mm";
+  const format = "HH:mm:ss";
   const reservation = moment(reservation_time, format);
-  const openTime = moment("10:30", format);
-  const lastCall = moment("21:30", format);
-  const closeTime = moment("22:30", format);
+  const openTime = moment("10:30:00", format,true).format();
+  const lastCall = moment("21:30:00", format, true).format();
+  const closeTime = moment("22:30:00", format, true).format();
 
 
 //middleware for various time and date contraints  
